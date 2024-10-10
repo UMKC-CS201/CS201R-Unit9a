d04503a22ece0f83c995c26df93aac596214a15a
Joan Gladbach <jjgr29@umsystem.edu>
2024-10-10 10:57:43 -0500

    update

--- CS201R-Unit9a/CS201R-Unit9a.cpp
+++ CS201R-Unit9a/CS201R-Unit9a.cpp
@@ -1,6 +1,7 @@
 // CS201R-Unit9a.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-//see bit.ly/CS201Wk9bOperator
+//https://github.com/UMKC-CS201/CS201R-Unit9a
+
 #include "Examples.h"
 
 int main() {
